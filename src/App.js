824e9abc8c4d002c73c8b9ce2f2755e02b4b1bfa
MAbdurahman <mdbdrrhm@gmail.com>
2020-10-02 07:48:39 -0500

    Add initial React setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/*===========================================
+            App.js
+===========================================*/
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>react-cocktail-search</h1>
+    </div>
+  );
+}
+
+export default App;
