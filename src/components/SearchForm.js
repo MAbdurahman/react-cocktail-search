81137431688c736ff88535db753812dde0eea5eb
MAbdurahman <mdbdrrhm@gmail.com>
2020-10-04 10:18:45 -0500

    Complete SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,14 +1,44 @@
 /*===========================================
             components/SearchForm.js
 ===========================================*/
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-const SearchForm = ({setSearchTerm}) => {
-   return (
-      <div>
-         <h1>components/SearchForm.js</h1>
-      </div>
-   )
-}
+const SearchForm = ({ setSearchTerm }) => {
+	//**************** Hook useRef ****************//
+	const searchValue = useRef('');
+
+	//**************** Hook useEffect ****************//
+	useEffect(() => {
+      searchValue.current.focus();
+      
+   }, []);
+   
+	//**************** Functionality ****************//
+	function searchCocktail() {
+		setSearchTerm(searchValue.current.value);
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+	}
+
+	return (
+		<section className='section'>
+			<h2 className='section-title'>search cocktails</h2>
+			<form className='form search-form' onSubmit={handleSubmit}>
+				<div className='form-control'>
+					<label htmlFor='name'>search your favorite cocktail</label>
+					<input
+						type='text'
+						name='name'
+						id='name'
+						ref={searchValue}
+						onChange={searchCocktail}
+					/>
+				</div>
+			</form>
+		</section>
+	);
+};
 
 export default SearchForm;
